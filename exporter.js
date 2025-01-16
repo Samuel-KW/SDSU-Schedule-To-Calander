312f78e9842a3a6364ee66e4fe36c72ed9d563f7
Samuel-UC <68044712+Samuel-KW@users.noreply.github.com>
2025-01-16 01:56:24 -0800

    fix: ics issues

--- exporter.js
+++ exporter.js
@@ -4,6 +4,54 @@ const CourseStatus = Object.freeze({
     WAITLIST: 1
 });
 
+const Timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+/**
+ * Converts a UTC date into the proper format for ICS calendar files for DTSTAMP, DTSTART and DTEND
+ * https://gist.github.com/mstudio/1d57356d90815cdf5a56ed873d41ce6d
+ * @param {Date} date, e.g. new Date()
+ * @returns {String} formatted as UTC date for ICS files, e.g. 20220511T170000Z
+ */
+const toICS = (date) => {
+    const dateArray = date.toISOString().split("T");
+    const day = dateArray[0].replace(/-/g, "");
+    const time = dateArray[1].replace(/[a-zA-Z]+/g, "").split(":");
+    const ics = `${day}T${getDigits(time[0])}${getDigits(time[1])}${getDigits(
+        time[2],
+    )}Z`;
+    return ics;
+};
+
+/**
+ * https://gist.github.com/mstudio/1d57356d90815cdf5a56ed873d41ce6d
+ * @param {String} timeString, e.g. '2.19'
+ * @returns {String} 2 digit string, e.g. '02'
+ */
+const getDigits = (timeString) => {
+    let digits = String(Math.round(parseInt(timeString, 10)));
+    if (digits.length === 1) digits = `0${digits}`;
+    return digits;
+};
+
+/**
+ * Downloads a calander (ICS format) with the given text as its contents
+ * @param {String} text - The contents of the file to be downloaded
+ */
+const download = (text) => {
+    const blob = new Blob([text], { type: "text/calendar" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "calender";
+
+    document.body.appendChild(link);
+    link.click();
+
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
+
 class Course {
 
     elemContainer;
@@ -127,6 +175,27 @@ class Course {
         }
     }
 
+    toICS () {
+        const start = toICS(this.start);
+        const end = toICS(this.end);
+        const rrule = "FREQ=WEEKLY;BYDAY=" + this.days.map(e => e.slice(0, 2)).join(",");
+
+        return `BEGIN:VEVENT
+UID:${this.title.replace(/[^a-zA-Z0-9]/g, "")}@samuel-kw
+SUMMARY:${this.room} (${this.title})
+DESCRIPTION:${this.title}
+LOCATION:${this.room}
+DTSTART:${start}
+DTEND:${end}
+DTSTAMP:${start}
+RRULE:${rrule}
+CREATED:${toICS(new Date())}
+PRIORITY:0
+STATUS:CONFIRMED
+END:VEVENT
+`;
+    }
+
     toString () {
         return `${this.title} (${this.room})
     Days: ${this.days?.join(", ")}
@@ -138,8 +207,19 @@ class Course {
     }
 }
 
+let str = `BEGIN:VCALENDAR
+PRODID:-//Samuel KW//SDSU Class Exporter 0.0.1
+VERSION:2.0
+CALSCALE:GREGORIAN
+`;
+
 const elemCourses = document.querySelectorAll("div.ps_box-group > div.ps_box-scrollarea > div.ps_box-scrollarea-row[id^=win11divSSR_SBJCT_LVL1_row]");
 for (const elem of elemCourses) {
     const course = new Course(elem);
     console.log(course.toEvent());
+
+    if (course.room)
+        str += course.toICS();
 }
+
+download(str + "END:VCALENDAR")
